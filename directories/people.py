90c55b98d48907cb2f5698820ef19be24ae1a95c
jakecreps <crepschrome@gmail.com>
2019-05-31 12:28:25 -0500

    added download options

--- directories/people.py
+++ directories/people.py
@@ -4,18 +4,100 @@ from colorama import Fore
 import webbrowser
 import time
 from . import menu
+import subprocess
 
 def people():
     time.sleep(0.5)
     toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+    print()
     if toolSelect == "1":
-        webbrowser.open('https://github.com/xillwillx/skiptracer') # Go to SkipTracer GitHub page
+        print(Fore.RED + " SkipTracer - scrape PII paywall sites on a ramen noodle budget")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/xillwillx/skiptracer')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading SkipTracer")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/xillwillx/skiptracer.git'])
+        elif view_or_download == "<":
+            menu.menu()
     elif toolSelect == "2":
-        webbrowser.open('https://github.com/lulz3xploit/LittleBrother') # Go to LittleBrother GitHub page
+        print(Fore.RED + " LittleBrother - information gathering tool for EU persons")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/lulz3xploit/LittleBrother')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading LittleBrother")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/lulz3xploit/LittleBrother.git'])
+        elif view_or_download == "<":
+            menu.menu()
     elif toolSelect == "3":
-        webbrowser.open('https://github.com/iojw/socialscan') # Go to SocialScan GitHub page
+        print(Fore.RED + " SocialScan - check email addresses across all social platforms")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/iojw/socialscan')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading SocialScan")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/iojw/socialscan.git'])
+        elif view_or_download == "<":
+            menu.menu()
     elif toolSelect == "4":
-        webbrowser.open('https://github.com/sherlock-project/sherlock') # Go to Sherlock Github page
+        print(Fore.RED + " Sherlock - search username availability across all social platforms")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/sherlock-project/sherlock')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading Sherlock")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/sherlock-project/sherlock.git'])
+        elif view_or_download == "<":
+            menu.menu()
     elif toolSelect == "<":
         menu.menu()
     people()
--- directories/recon.py
+++ directories/recon.py
@@ -4,22 +4,150 @@ from colorama import Fore
 import webbrowser
 import time
 from . import menu
+import subprocess
 
 def recon():
     time.sleep(0.5)
     toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+    print()
     if toolSelect == "1":
-        webbrowser.open('https://github.com/s0md3v/Photon') # Go to Photon GitHub page
+        print(Fore.RED + " Photon - an incredibly fast crawler designed for OSINT")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.75)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/s0md3v/Photon')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading Photon")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/s0md3v/Photon.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "2":
-        webbrowser.open('https://github.com/twelvesec/gasmask') # Go to GasMask GitHub page
+        print(Fore.RED + " GasMask - an all in one OSINT gathering tool")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/twelvesec/gasmask')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading GasMask")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/twelvesec/gasmask.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "3":
-        webbrowser.open('https://github.com/woj-ciech/kamerka') # Go to Kamerka GitHub page
+        print(Fore.RED + " Kamerka - build an interactive map of cameras from Shodan")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/woj-ciech/kamerka')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading Kamerka")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/woj-ciech/kamerka.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "4":
-        webbrowser.open('https://github.com/thewhiteh4t/FinalRecon') # Go to FinalRecon GitHub page
+        print(Fore.RED + " FinalRecon - an OSINT tool for all in one web reconnaissance")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/thewhiteh4t/FinalRecon')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading Kamerka")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/thewhiteh4t/FinalRecon.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "5":
-        webbrowser.open('https://spiderfoot.net') # Go to SpiderFoot homepage
+        print(Fore.RED + " SpiderFoot - automate OSINT from hundreds of sources")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/smicallef/spiderfoot')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading Spiderfoot")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/smicallef/spiderfoot.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "6":
-        webbrowser.open('https://github.com/netevert/pockint') # Go to SpiderFoot homepage
+        print(Fore.RED + " POCKINT - a portable OSINT Swiss Army Knife for DFIR professionals")
+        print()
+        options = [
+        Fore.CYAN + " [1]" + Fore.WHITE + " View the tool",
+        Fore.CYAN + " [2]" + Fore.WHITE + " Download the tool",
+        Fore.CYAN + " [<]" + Fore.WHITE + " Main menu"
+        ]
+        print(*options,sep='\n')
+        print()
+        time.sleep(0.5)
+        view_or_download = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+        print()
+        if view_or_download == "1":
+            webbrowser.open('https://github.com/netevert/pockint')
+        elif view_or_download == "2":
+            print(Fore.CYAN + " [+] " + Fore.WHITE + "Downloading POCKINT")
+            time.sleep(0.5)
+            print()
+            subprocess.call(['git', 'clone', 'https://github.com/netevert/pockint.git'])
+        elif view_or_download == "<":
+            menu.menu()
+
     elif toolSelect == "<":
         menu.menu()
     recon()
